a7ebab70d4c5d11475a78a23c711e3896604750e
rollervibeslapaz-eng <rollervibeslapaz@gmail.com>
2026-05-27 01:18:38 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "gym-app-cache-v2";
+const CACHE_NAME = "gym-app-cache-v10";
 
 const urlsToCache = [
   "./",
@@ -9,33 +9,37 @@ const urlsToCache = [
 ];
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
 
   event.waitUntil(
-
     caches.open(CACHE_NAME)
-
     .then(cache => {
-
       return cache.addAll(urlsToCache);
-
     })
+  );
+});
 
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
   );
 
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-
   event.respondWith(
-
-    caches.match(event.request)
-
-    .then(response => {
-
-      return response || fetch(event.request);
-
+    fetch(event.request)
+    .catch(() => {
+      return caches.match(event.request);
     })
-
   );
-
 });
